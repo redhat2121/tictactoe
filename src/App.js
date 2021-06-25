b510b0ada98e5a8c37a26fdd02e5831deea409f3
redhat2121 <redhat2121@protonmail.com>
2021-06-25 16:41:14 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () =>{ 
+  return (
+  <>
+    <h1>Welcome to React Parcel Micro App!</h1>
+    <p>hii there!! </p>
+  </>
+  );
+  };
+ 
+export default App;
